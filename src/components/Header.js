df049d8fb59c43b75784d994d7b646ecd5f8458d
OpeDada <opedada@gmail.com>
2021-10-22 14:08:17 +0900

    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title, onAdd, showAdd }) => {
     <header className="header">
       <h1>{title}</h1>
       <Button
-        color={showAdd ? "red" : "green"}
+        color={showAdd ? "blue" : "green"}
         text={showAdd ? "Close" : "Add"}
         onClick={onAdd}
       />
@@ -16,7 +16,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 }
 
 Header.defaultProps = {
-  title: 'Task Tracker',
+  title: 'Task Manager',
 }
 
 Header.propTypes = {
